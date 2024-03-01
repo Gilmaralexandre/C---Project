a77c9d079e2bef2c324389222349f5de1ad3e61a
Gilmar <gilmar.alexandre.sc@outlook.com>
2024-02-29 22:46:39 -0300

    commit inicial

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿﻿// Screen Sound
+string mensagemDeBoasVindas = "Boas vindas ao Gasc Music";
+List<string> listaDasBandas = new List<string> { "U2", "The Beatles", "Calypso"};  
+
+void ExibirLogo()
+{
+    Console.WriteLine(
+        @"Ꮆ闩丂⼕ 爪ㄩ丂讠⼕"
+        );
+    Console.WriteLine(mensagemDeBoasVindas);
+}
+
+void ExibirOpcoesDoMenu()
+{
+    Console.WriteLine("\nDigite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda");
+    Console.WriteLine("Digite -1 para sair");
+
+    Console.WriteLine("\nDigite sua opção:");
+}
+
+ExibirLogo();
+ExibirLogo();
+ExibirOpcoesDoMenu();
+
+Console.ReadLine();
